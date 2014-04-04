b4981aa4f943f935e80975da1f8db479eafc21ee
Lucas Churchill <lucas@agtlucas.com>
2014-04-03 21:33:25 -0300

    Update WriterDecorator

--- WriterDecorator.rb
+++ WriterDecorator.rb
@@ -1,5 +1,9 @@
 # WriterDecorator
 
+=begin
+The Forwardable module provides delegation of specified methods to a designated object, using the methods def_delegator and def_delegators.
+http://ruby-doc.org/stdlib-1.9.3/libdoc/forwardable/rdoc/Forwardable.html
+=end
 require 'forwardable'
 
 class WriterDecorator
